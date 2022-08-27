93bcb017c0c9abaf01ad64a862ee35d83b4854f6
MRNoman <na2458443@gmail.com>
2022-08-27 18:25:17 +0500

    after foam data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,9 @@
 const User = require("../models/userModels");
-
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 exports.signup = async (req, res) => {
   try {
     const user = await User.create({
@@ -8,7 +12,7 @@ exports.signup = async (req, res) => {
       username: req.body.username,
     });
     res.status(200).json({ data: user, msg: "Success" });
-    // res.send(user);
+    res.send(user);
   } catch (error) {
     console.log(error.message);
     res.status(404).json({ error: error.message });
